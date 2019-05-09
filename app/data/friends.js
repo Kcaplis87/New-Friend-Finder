574ad2c20d7f57c666c8a2e00b6e52fc58b79208
Kyle Caplos <kylemcaplis@hotmail.com>
2019-05-09 18:34:16 -0400

    working product

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+
+
+
+var friends = [{
+
+    name: "Bball guy",
+
+    photo: "https://images.pexels.com/photos/2019005/pexels-photo-2019005.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [1, 2, 4, 2, 5, 2, 2, 3, 2, 1]
+
+}, {
+
+    name: "Blue Football Team",
+
+    photo: "https://images.pexels.com/photos/186076/pexels-photo-186076.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [1, 4, 3, 4, 4, 3, 1, 3, 2, 4]
+
+}, {
+
+    name: "Golfer Guy",
+
+    photo: "https://images.pexels.com/photos/33478/golf-sunset-sport-golfer.png?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [4, 3, 4, 2, 4, 3, 3, 4, 4, 1]
+
+}, {
+
+    name: "Tennis Girl",
+
+    photo: "https://images.pexels.com/photos/1772724/pexels-photo-1772724.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [2, 4, 1, 3, 2, 1, 2, 5, 2, 3]
+
+}, {
+
+    name: "Cricket Guy",
+
+    photo: "https://images.pexels.com/photos/1594943/pexels-photo-1594943.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [5, 5, 1, 1, 5, 1, 5, 1, 3, 1]
+
+}, {
+
+    name: "Goalie Guy",
+
+    photo: "https://images.pexels.com/photos/159636/soccer-football-soccer-player-goal-keeper-159636.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [1, 3, 4, 2, 5, 4, 1, 3, 1, 4]
+
+}, {
+
+    name: "Little Skier",
+
+    photo: "https://images.pexels.com/photos/848618/pexels-photo-848618.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+
+    scores: [1, 3, 2, 2, 4, 3, 3, 2, 5, 4]
+
+}];
+
+module.exports = friends;
